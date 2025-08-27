ebe1b2ad46d7943800a63691e21c2c82f1939c40
Nikhil-X-codes <nagarn2005@gmail.com>
2025-08-27 19:42:30 +0530

    chore: Add compression middleware to backend for improved performance
    - Included compression package in package.json and package-lock.json
    - Updated app.js to import and utilize compression middleware

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-import cookieParser from 'cookie-parser';           //middleware to parse cookies
+import cookieParser from 'cookie-parser';          
 import compression from 'compression';
 
 
